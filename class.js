f4e95240eb218e8950560a56a7a4f1369c68c4e3
André Moreira <112489349+andremoreiradev@users.noreply.github.com>
2024-01-04 16:46:11 -0300

    Update class.js
    Class de um jogo;

--- class.js
+++ class.js
@@ -1,1 +1,36 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
 
+  atacar() {
+    console.log(`o ${this.tipo} atacou usando ${this.tipoAtaque()}}`);
+  }
+
+  private tipoAtaque() {
+    switch (this.tipo) {
+      case "mago":
+        return "magia";
+      case "guerreiro":
+        return "espada";
+      case "monge":
+        return "artes marciais";
+      case "ninja":
+        return "shuriken";
+      default:
+        return "desconhecido";
+    }
+  }
+}
+
+const heroi1 = new Heroi("Aragon", 30, "guerreiro");
+const heroi2 = new Heroi("Gandalf", 1000, "mago");
+const heroi3 = new Heroi("Bruce Lee", 40, "monge");
+const heroi4 = new Heroi("Kakashi", 27, "ninja");
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
